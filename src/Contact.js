d685808c5c05db8b1f0be614848c1fb2574b8f2e
Reena Raghavan <reena.raghavan@yahoo.com>
2020-07-17 17:56:06 +0530

    First commit

---
+++ src/Contact.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+export const Contact = () => (
+  <div>
+    <h2>Contact Us</h2>
+    <p>
+      Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus risus
+      erat, commodo ac ultricies quis, blandit at erat. In blandit orci massa,
+      id eleifend erat imperdiet sit amet. Etiam pharetra tincidunt fringilla.
+      Fusce nisl ipsum, sagittis at felis vitae, consectetur dapibus ipsum.
+      Vivamus nec felis iaculis, vehicula orci in, elementum neque. Phasellus
+      non risus et purus pulvinar facilisis sed in erat. Ut fermentum, purus
+      quis posuere blandit, turpis elit tempor justo, a porttitor nulla magna
+      nec velit. Proin tempus volutpat fringilla.
+    </p>
+  </div>
+);
---
+++ src/Home.js
@@ -0,0 +1,37 @@
+import React from "react";
+
+export const Home = () => (
+  <div>
+    <h2>Hello World</h2>
+    <p>
+      Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus risus
+      erat, commodo ac ultricies quis, blandit at erat. In blandit orci massa,
+      id eleifend erat imperdiet sit amet. Etiam pharetra tincidunt fringilla.
+      Fusce nisl ipsum, sagittis at felis vitae, consectetur dapibus ipsum.
+      Vivamus nec felis iaculis, vehicula orci in, elementum neque. Phasellus
+      non risus et purus pulvinar facilisis sed in erat. Ut fermentum, purus
+      quis posuere blandit, turpis elit tempor justo, a porttitor nulla magna
+      nec velit. Proin tempus volutpat fringilla.
+    </p>
+    <p>
+      Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus risus
+      erat, commodo ac ultricies quis, blandit at erat. In blandit orci massa,
+      id eleifend erat imperdiet sit amet. Etiam pharetra tincidunt fringilla.
+      Fusce nisl ipsum, sagittis at felis vitae, consectetur dapibus ipsum.
+      Vivamus nec felis iaculis, vehicula orci in, elementum neque. Phasellus
+      non risus et purus pulvinar facilisis sed in erat. Ut fermentum, purus
+      quis posuere blandit, turpis elit tempor justo, a porttitor nulla magna
+      nec velit. Proin tempus volutpat fringilla.
+    </p>
+    <p>
+      Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus risus
+      erat, commodo ac ultricies quis, blandit at erat. In blandit orci massa,
+      id eleifend erat imperdiet sit amet. Etiam pharetra tincidunt fringilla.
+      Fusce nisl ipsum, sagittis at felis vitae, consectetur dapibus ipsum.
+      Vivamus nec felis iaculis, vehicula orci in, elementum neque. Phasellus
+      non risus et purus pulvinar facilisis sed in erat. Ut fermentum, purus
+      quis posuere blandit, turpis elit tempor justo, a porttitor nulla magna
+      nec velit. Proin tempus volutpat fringilla.
+    </p>
+  </div>
+);
